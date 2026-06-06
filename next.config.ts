13175ed454ae98c2986f742db485b72912dd56d7
Tanvir A <tanvir@usarus.co.nz>
2026-06-06 15:18:38 +0600

    feat: setup static export and github pages pipeline

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // Tells Next.js to generate a static 'out' folder
+  images: {
+    unoptimized: true, // Required because Next.js default image optimization needs a live server
+  },
 };
 
 export default nextConfig;
